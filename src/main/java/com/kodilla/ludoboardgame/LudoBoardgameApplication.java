88f09de9d1e873183e06ba7f0f42c42e2b402984
Skorpien <30352529+Skorpien@users.noreply.github.com>
2019-09-05 13:06:23 +0200

    first commit

---
+++ src/main/java/com/kodilla/ludoboardgame/LudoBoardgameApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.ludoboardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudoBoardgameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LudoBoardgameApplication.class, args);
+    }
+
+}
